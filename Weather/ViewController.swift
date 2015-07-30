70fadba74076d0a025d26cce18b82b847f3ce568
Diji Adu <dijiadu@users.noreply.github.com>
2015-07-30 21:29:11 +0100

    working example

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    // Class vars
+    
+    let locationManager = CLLocationManager()
+    let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=%.2f&lon=%.2f&units=metric"
+    //http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric
+    
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
 
+    override func viewDidAppear(animated: Bool) {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Location Manager
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        locationManager.stopUpdatingLocation()
+        getWeatherData(locationManager.location!.coordinate)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        NSLog("Location ERROR: %@", error.localizedDescription)
+    }
+    
+    // MARK: Weather
+    
+    func getWeatherData(coordinates: CLLocationCoordinate2D) {
+        let locationUrlString = String(format: urlString, coordinates.latitude, coordinates.longitude)
+        let url = NSURL(string: locationUrlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler:{(data, response, error)->Void in
+            dispatch_async(dispatch_get_main_queue(), {
+                if error == nil {
+                    self.setLabels(data)
+                } else {
+                    NSLog("Request ERROR: %@", error.localizedDescription)
+                }
+            })
+        })
+        
+        task.resume()
+    }
+    
+    func setLabels(weatherData: NSData) {
+        var jsonError: NSError?
+        
+        if let json = NSJSONSerialization.JSONObjectWithData(weatherData, options: nil, error: &jsonError) as? NSDictionary {
+            if let name = json["name"] as? String {
+                cityLabel.text = name
+            }
+            
+            if let main = json["main"] as? NSDictionary {
+                if let temp = main["temp"] as? Double {
+                    tempLabel.text = String(format: "%.0f℃", temp)
+                }
+            }
+            
+            if let weather = json["weather"] as? NSArray {
+                if let weatherDict = weather[0] as? NSDictionary {
+                    if let main = weatherDict["main"] as? String {
+                        weatherLabel.text = main
+                    }
+                    
+                    if let icon = weatherDict["icon"] as? String {
+                        weatherImage.image = UIImage(named: icon)
+                    }
+                }
+            }
+        } else {
+            NSLog("Parse ERROR: %@", jsonError!.localizedDescription)
+        }
+    }
 }
 
--- WeatherTests/WeatherTests.swift
+++ WeatherTests/WeatherTests.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 import XCTest
+import Weather
+import CoreLocation
 
 class WeatherTests: XCTestCase {
+    
+    var vc: ViewController!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        var storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: self.dynamicType))
+        vc = storyboard.instantiateViewControllerWithIdentifier("WeatherVC") as! ViewController
+        vc.loadView()
     }
     
     override func tearDown() {
@@ -21,15 +29,29 @@ class WeatherTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testViewDidLoad() {
+        // assert that the ViewController.view is not nil
+        XCTAssertNotNil(vc.view, "View Did Not load")
     }
     
-    func testPerformanceExample() {
+    func testSetLabels() {
+        let json = "{\"coord\":{\"lon\":138.93,\"lat\":34.97},\"weather\":[{\"id\":801,\"main\":\"Clouds\",\"description\":\"few clouds\",\"icon\":\"02n\"}],\"base\":\"stations\",\"main\":{\"temp\":26.52,\"pressure\":1009,\"humidity\":96,\"temp_min\":26.11,\"temp_max\":27.22},\"wind\":{\"speed\":1.96,\"deg\":226.004},\"rain\":{},\"clouds\":{\"all\":12},\"dt\":1438284810,\"sys\":{\"type\":3,\"id\":10294,\"message\":0.0197,\"country\":\"JP\",\"sunrise\":1438199534,\"sunset\":1438249723},\"id\":1851632,\"name\":\"Shuzenji\",\"cod\":200}"
+        
+        if let data = json.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+            vc.setLabels(data)
+        }
+        
+        XCTAssertTrue(vc.cityLabel.text == "Shuzenji", "cityLabel is not set to 'Shuzenji', but to \(vc.cityLabel.text)")
+        XCTAssertTrue(vc.weatherLabel.text == "Clouds", "weatherLabel is not set to 'clouds', but to \(vc.weatherLabel.text)")
+        XCTAssertTrue(vc.tempLabel.text == "27℃", "tempLabel is not set to '27℃', but to \(vc.tempLabel.text)")
+    }
+    
+    func testPerformanceOfGetWeather() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
+            let coordinates = CLLocationCoordinate2D(latitude: 35, longitude: 139)
+            self.vc.getWeatherData(coordinates)
         }
     }
     
